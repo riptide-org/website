320ee1d235345003fd5e4138f003fc19beaef042
Lachlan Davidson <lachlan@lachlandavidson.com>
2021-09-30 19:01:50 +1300

    feat: Error handling in file page

--- pages/f/_serverid/_fileid.vue
+++ pages/f/_serverid/_fileid.vue
@@ -1,14 +1,22 @@
 <template>
-  <pre>{{ JSON.stringify(fileData) }}</pre>
+  <pre>{{ JSON.stringify(fileData, null, 4) }}</pre>
 </template>
 
 <script>
 export default {
-  async asyncData({ params, $axios }) {
+  async asyncData({ params, error, $axios }) {
     const { serverid, fileid } = params
-    const fileData = await $axios.$get(`/api/v1/server/${serverid}/file/${fileid}`)
-    // todo: 404
-    return { fileData }
+    try {
+      const fileData = await $axios.$get(`/api/v1/server/${serverid}/file/${fileid}`)
+      return { fileData }
+    } catch ({ response }) {
+      // TODO: Handle server unavailble, once handled in API
+      if (response && response.status === 404) {
+        error({ statusCode: 404, customMessage: 'File Not Found' })
+      } else {
+        error({ statusCode: 500 })
+      }
+    }
   }
 }
 </script>
