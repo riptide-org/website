8e47fe69add02385b57b11797fe3e3a1cbfb4d27
Lachlan Davidson <lachlan@lachlandavidson.com>
2021-09-30 19:01:19 +1300

    feat: Added error page

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2 class="text-2xl leading-7 font-semibold text-center">
+      <span v-if="error.customMessage != null">{{ error.customMessage }}</span>
+      <span v-else-if="error.statusCode === 404">Page Not Found</span>
+      <span v-else>An Error Occured</span>
+    </h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['error'],
+    layout: 'error'
+  }
+</script>
